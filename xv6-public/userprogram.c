73f23aaa6129528f065780547af4bc8681b6b5ba
Jared Novoa <novoa@rockhopper-07.cs.wisc.edu>
2024-03-10 16:09:04 -0500

    working on wmap

--- xv6-public/userprogram.c
+++ xv6-public/userprogram.c
@@ -5,7 +5,10 @@
 
 int main(void) 
 {
-    uint address = wmap(0x60000000, 8192, MAP_ANONYMOUS, -1);
+    int flags = MAP_ANONYMOUS | MAP_FIXED;
+    printf(1, "flags from userprogram: %d\n", flags);
+    uint address = wmap(0x60000000, 8192, flags, -1);
+    // uint address = wmap(0x60000000, 8192, MAP_ANONYMOUS, -1);
     printf(1,"%x\n", address);
     exit();
 } 
