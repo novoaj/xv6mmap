1591150e926caf3b7490f0cadf6cb04e498e16b5
Jared Novoa <novoa@snares-08.cs.wisc.edu>
2024-03-11 17:34:29 -0500

    changes to userprogram.c for debugging inserts/wmaps

--- xv6-public/userprogram.c
+++ xv6-public/userprogram.c
@@ -8,10 +8,10 @@ int main(void)
     int flags = MAP_ANONYMOUS | MAP_FIXED;
     printf(1, "flags from userprogram: %d\n", flags);
     uint address = wmap(0x60000000, 8192, flags, -1);
-    printf(1,"%x\n", address);
+    printf(1,"return addr: %x\n", address);
     uint address2 = wmap(0x60000000, 8192, MAP_ANONYMOUS, -1);
-    printf(1,"%x\n", address2);
+    printf(1,"return addr: %x\n", address2);
     uint address3 = wmap(0x60000000, 10000, MAP_ANONYMOUS, -1);
-    printf(1,"%x\n", address3);
+    printf(1,"return addr: %x\n", address3);
     exit();
 } 
